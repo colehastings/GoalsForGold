a6211d50e2548d62202e3cd927da4e7c15e4ab41
colehastings <colehastings567@gmail.com>
2017-01-09 15:14:24 -0500

    Launch Screen and Welcome Screen

--- GoalsForGold/SplashScreen.swift
+++ GoalsForGold/SplashScreen.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        perform(#selector(SplashScreen.showNavController), with: nil, afterDelay: 3)
+        
 
+    }
+    func showNavController(){
+    performSegue(withIdentifier: "showSplashScreen", sender: self)
+       
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
